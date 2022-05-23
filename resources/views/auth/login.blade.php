ee544ce8e6f7969a70e44c647a04e79616105641
yamanekaito0630 <yamanekaito0630@gmail.com>
2022-05-23 20:09:23 +0900

    change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,14 +43,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <div>
-                    <div>
-                        @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                               href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-                    </div>
+
                     <div>
                         <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                             {{ __('Register') }}
